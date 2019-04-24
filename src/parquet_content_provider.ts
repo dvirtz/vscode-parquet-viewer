6bd6722ea46467dca71b884eb8b2d999ab16b802
Dvir Yitzchaki <dyitzchaki@oath.com>
2019-04-24 19:49:16 +0300

    replace exec with spawn to support large parquets. fixes #1

--- src/parquet_content_provider.ts
+++ src/parquet_content_provider.ts
@@ -1,24 +1,56 @@
 import { TextDocumentContentProvider, EventEmitter, Uri, window } from "vscode";
-import { exec } from "child_process";
+import { spawn } from "child_process";
+// import { Readable } from "stream";
+// import { tmpdir } from "os";
+// import { createWriteStream, readFile, readFileSync } from 'fs';
+// import { sep } from 'path';
+
+
+class Json {
+  data: string = "";
+}
 
 export class ParquetContentProvider implements TextDocumentContentProvider {
+
+  private jsons: Map<string, Json> = new Map();
 
   // emitter and its event
   onDidChangeEmitter = new EventEmitter<Uri>();
   onDidChange = this.onDidChangeEmitter.event;
 
   async provideTextDocumentContent(uri: Uri): Promise<string> {
-    // simply invoke cowsay, use uri-path as text
     return new Promise<string>((resolve, reject) => {
-      exec('parquet-tools cat -j ' + uri.path, (error, stdout, stderr) => {
-        if (error) {
-          const message = `error when running parquet-tools ${error}:\n${stderr}`;
+
+      const path = uri.path.replace(RegExp('\.as\.json$'), '');
+
+      if (this.jsons.has(path)) {
+        resolve(this.jsons.get(path)!.data);
+      }
+
+      var json = new Json;
+      this.jsons.set(path, json);
+
+      const parquet_tools = spawn('parquet-tools', ['cat', '-j', path]);
+      // parquet_tools.stdout.pipe(stream)
+      var stderr: string = "";
+      parquet_tools.stderr.on('data', (data) => {
+        stderr += data;
+      });
+      parquet_tools.stdout.on('data', (data) => {
+        json.data += data;
+        this.onDidChangeEmitter.fire(uri);
+      });
+
+      parquet_tools.on('close', (code) => {
+        if (code) {
+          const message = `error when running parquet-tools ${code}:\n${stderr}`;
           window.showErrorMessage(message);
           reject(message);
         }
 
-        resolve(stdout);
+        resolve(json.data);
       });
     });
   }
+
 }
