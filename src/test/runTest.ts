5c20f6d5295cdea383db01887981cb85a37e083a
Dvir Yitzchaki <dyitzchaki@oath.com>
2020-04-06 15:02:32 +0300

    audit and test fixes

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -6,17 +6,18 @@ async function main() {
   try {
     // The folder containing the Extension Manifest package.json
     // Passed to `--extensionDevelopmentPath`
-    const extensionPath = path.resolve(__dirname, '../..');
+    const extensionDevelopmentPath = path.resolve(__dirname, '../..');
 
     // The path to the extension test runner script
     // Passed to --extensionTestsPath
-    const testRunnerPath = path.resolve(__dirname, './suite/index');
+    const extensionTestsPath = path.resolve(__dirname, './suite/index');
 
-    const testWorkspace = path.resolve(extensionPath, './src/test/workspace/test.code-workspace');
+    const launchArgs = [path.resolve(extensionDevelopmentPath, './src/test/workspace/test.code-workspace')];
 
     // Download VS Code, unzip it and run the integration test
-    await runTests({ extensionPath, testRunnerPath, testWorkspace });
+    await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs});
   } catch (err) {
+    console.error(err);
     console.error('Failed to run tests');
     process.exit(1);
   }
