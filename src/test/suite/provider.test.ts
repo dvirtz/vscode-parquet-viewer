1876bf7c8cad6add7edc8b711fba8f0e65e338ac
Dvir Yitzchaki <dyitzchaki@oath.com>
2020-04-10 17:07:00 +0300

    use chai for testing

--- src/test/suite/provider.test.ts
+++ src/test/suite/provider.test.ts
@@ -1,41 +1,38 @@
 
-import * as assert from 'assert';
-
 import { ParquetContentProvider } from "../../parquet_content_provider";
 import { getUri, fileRead } from './utils';
 import { Uri } from 'vscode';
+import { expect, use } from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+
+use(chaiAsPromised);
 
 suite("Provider tests", () => {
   const provider = new ParquetContentProvider();
 
-  test('Converts Parquet to JSON', async () => {
-    await Promise.all(["small", "large"].map(async name => {
+  test('Converts Parquet to JSON', function() {
+    this.timeout(20000);
+    return Promise.all(["small", "large"].map(async name => {
       const json = await getUri(`${name}.parquet`).then(parquet => {
         return provider.provideTextDocumentContent(parquet);
       });
       const expected = await fileRead(`${name}.json`);
 
       if (name === "small") {
-        assert.strictEqual(json, expected);
-      } else if (json !== expected){
-        assert.fail("large JSON differ");
+        expect(json).to.equal(expected);
+      } else if (json !== expected) {
+        expect.fail("large JSON differ");
       }
     }));
   });
 
-  test("Error on not existing file", async () => {
-    await provider.provideTextDocumentContent(Uri.file("."))
-      .catch(error => {
-        assert(error.indexOf('error when running parquet-tools') !== -1);
-      });
+  test("Error on not existing file", function () {
+    expect(provider.provideTextDocumentContent(Uri.file("."))).to.be.rejectedWith(
+      "error when running parquet-tools 1:\njava.io.FileNotFoundException: / (Is a directory)\n\n"
+    );
   });
 
-  test("-h works", async () => {
-    await ParquetContentProvider.spawnParquetTools(['-h']).then(process =>
-      process
-        .on('error', err => assert.fail('got error ' + err))
-        .on('close', code => {
-          assert.equal(code, 0);
-        }));
+  test("-h works", function () {
+    expect(ParquetContentProvider.spawnParquetTools(['-h'])).to.eventually.equal(0);
   });
 });
