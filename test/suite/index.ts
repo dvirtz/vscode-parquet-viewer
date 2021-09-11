4b40f5ae3a679eba39e11d8f16af4c46ecb6dc21
dvirtz <dvirtz@gmail.com>
2021-09-11 23:39:48 +0300

    upgrade to vscode/test-electron

--- test/suite/index.ts
+++ test/suite/index.ts
@@ -1,9 +1,10 @@
 import * as path from 'path';
 import * as Mocha from 'mocha';
-import * as glob from 'glob';
+import * as glob from 'glob-promise';
 import { env } from 'process';
+import { commands } from 'vscode';
 
-export function run(): Promise<void> {
+export async function run(): Promise<void> {
   // Create the mocha test
   const options: Mocha.MochaOptions = {
     ui: 'tdd',
@@ -18,27 +19,24 @@ export function run(): Promise<void> {
 
   const testsRoot = path.resolve(__dirname, '..');
 
-  return new Promise((c, e) => {
-    glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-      if (err) {
-        return e(err);
-      }
+  const files = await glob('**/**.test.js', { cwd: testsRoot });
 
-      // Add files to the test suite
-      files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+  // Add files to the test suite
+  files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
 
-      try {
-        // Run the mocha test
-        mocha.run(failures => {
-          if (failures > 0) {
-            e(new Error(`${failures} tests failed.`));
-          } else {
-            c();
-          }
-        });
-      } catch (err) {
-        e(err);
-      }
+  try {
+    // Run the mocha test
+    return await new Promise((c, e) => {
+      // Run the mocha test
+      mocha.run(failures => {
+        if (failures > 0) {
+          e(new Error(`${failures} tests failed.`));
+        } else {
+          c();
+        }
+      });
     });
-  });
+  } finally {
+    await commands.executeCommand("workbench.action.closeAllEditors");
+  }
 }
