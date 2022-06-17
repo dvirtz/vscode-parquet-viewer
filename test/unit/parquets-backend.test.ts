5a61a8e52494718e469243e0d34923a9727ad59a
dvirtz <dvirtz@gmail.com>
2022-06-17 18:21:32 +0300

    feat: formatting JSON output
    closes #43

--- test/unit/parquets-backend.test.ts
+++ test/unit/parquets-backend.test.ts
@@ -4,6 +4,7 @@ import { createReadStream } from 'fs';
 import * as path from 'path';
 import { createInterface } from 'readline';
 import { ParquetsBackend } from '../../src/parquets-backend';
+import { jsonSpace } from '../../src/settings';
 
 
 const rootDir = path.join(__dirname, '..', '..');
@@ -20,13 +21,15 @@ jest.mock('vscode', () => {
   };
 });
 
+jest.mock('../../src/settings');
+
 describe("ParquetsBackend tests", () => {
   const backend = new ParquetsBackend();
+  const workspace = path.join(rootDir, 'test', 'workspace');
 
   test.each(
     ["small", "large"]
   )('Converts %s parquet to JSON', async function (name) {
-    const workspace = path.join(rootDir, 'test', 'workspace');
     const json = (await toArray(backend.toJson(path.join(workspace, `${name}.parquet`)))).map(line => line.trim());
     const expected = await toArray(createInterface({input: createReadStream(path.join(workspace, `${name}.json`))}));
 
@@ -37,5 +40,15 @@ describe("ParquetsBackend tests", () => {
     await expect(toArray(backend.toJson("no-such-file"))).rejects.toMatchObject({
       'message': expect.stringMatching(/while reading no-such-file: Error: ENOENT: no such file or directory, stat '.*no-such-file'/)
     });
+  });
+
+  test.each([0, 2, 10, "\t", "###"])('Test space %s', async function (space) {
+    jest.mocked(jsonSpace).mockReturnValue(space);
+
+    const json = (await toArray(backend.toJson(path.join(workspace, `small.parquet`)))).map(line => line.trim());
+    const records = await toArray(createInterface({input: createReadStream(path.join(workspace, `small.json`))}));
+    const expected = records.map(record => JSON.stringify(JSON.parse(record), null, space));
+
+    expect(json).toEqual(expected);
   });
 });
