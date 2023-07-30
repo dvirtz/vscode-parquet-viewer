2774fe1c78415b0277180a74678e99c49291e542
dvirtz <dvirtz@gmail.com>
2023-07-30 02:15:56 +0100

    fix: import arrow dynamically to avoid failed loading
    error will only be reported when using arrow backend
    
    Fixes #83

--- src/arrow-backend.ts
+++ src/arrow-backend.ts
@@ -3,7 +3,7 @@ import { PassThrough } from 'stream';
 import { CancellationToken } from 'vscode';
 import { ParquetBackend } from './parquet-backend';
 import { jsonSpace } from './settings';
-import { readParquet } from 'parquet-reader';
+import { Stream } from 'stream';
 
 function bigIntToJson(value: bigint) {
   // serialize as a number if it's in bounds, otherwise as a string
@@ -14,9 +14,23 @@ function bigIntToJson(value: bigint) {
 }
 
 export class ArrowBackend extends ParquetBackend {
+  readParquet_: ((path: string, stream: Stream) => void) | undefined;
+
+  private async readParquet(path: string, stream: Stream) {
+    if (typeof (this.readParquet_) == 'undefined') {
+      try {
+        const module = await import("parquet-reader");
+        this.readParquet_ = module.readParquet;
+      } catch (_) {
+        throw new Error('cannot find prebuilt arrow module, either build the module or use another backend');
+      }
+    }
+    this.readParquet_(path, stream);
+  }
+
   public async * toJsonImpl(parquetPath: string, _token?: CancellationToken): AsyncGenerator<string> {
     const stream = new PassThrough;
-    readParquet(parquetPath, stream);
+    await this.readParquet(parquetPath, stream);
     const batches = await AsyncRecordBatchStreamReader.from(stream);
 
     // read all records from the file and print them
