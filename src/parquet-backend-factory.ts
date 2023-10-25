517d43cfbbe93c96ab2bb13e9f4382d11a5c956e
dvirtz <dvirtz@gmail.com>
2023-10-25 21:54:08 +0100

    chore: add logging

--- src/parquet-backend-factory.ts
+++ src/parquet-backend-factory.ts
@@ -1,9 +1,11 @@
 import { ArrowBackend } from './arrow-backend';
 import { BackendName } from './backend-name';
+import { getLogger } from './logger';
 import { ParquetToolsBackend } from './parquet-tools-backend';
 import { ParquetsBackend } from './parquets-backend';
 
 export function createParquetBackend(backend: BackendName) {
+  getLogger().info(`using ${backend} backend`);
   switch (backend) {
     case 'parquet-tools':
       return new ParquetToolsBackend;
