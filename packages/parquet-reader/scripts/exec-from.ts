328247c2a2f497c4c05c7c11a53d22be63af58a8
dvirtz <dvirtz@gmail.com>
2024-04-02 12:28:15 +0100

    build: simplify prebuilds by building inside dist

--- packages/parquet-reader/scripts/exec-from.ts
+++ packages/parquet-reader/scripts/exec-from.ts
@@ -1,22 +1,16 @@
-import {spawnSync} from 'child_process';
+import { spawnSync } from 'child_process';
 
-const proc_name = process.argv.shift();
-const script_name = process.argv.shift();
-const working_dir = process.argv.shift();
-if (working_dir) {
-  const cmd = process.argv.shift();
-  if (cmd) {
-    const err = spawnSync(cmd, process.argv, {
-      cwd: working_dir,
-      stdio: 'inherit',
-      shell: true
-    }).error;
-    if (err) {
-      throw err;
-    }
-    process.exit(0);
-  }
+if (process.argv.length < 4) {
+  console.error(`usage: ${process.argv0} working_dir cmd [args...]`);
+  process.exit(1);
 }
 
-console.error(`usage: ${proc_name} ${script_name} working_dir cmd [args...]`)
-process.exit(1);
+const [working_dir, cmd, ...args] = process.argv.slice(2);
+const err = spawnSync(cmd, args, {
+  cwd: working_dir,
+  stdio: 'inherit',
+  shell: true
+}).error;
+if (err) {
+  throw err;
+}
