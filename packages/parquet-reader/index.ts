8b94ae28abe2263167f24a80339d40fa16acfe7a
dvirtz <dvirtz@gmail.com>
2024-04-12 13:34:23 +0100

    fix: look for prebuilds in the root path
    fixes https://github.com/dvirtz/vscode-parquet-viewer/issues/121

--- packages/parquet-reader/index.ts
+++ packages/parquet-reader/index.ts
@@ -1,10 +1,13 @@
 import bindings from 'pkg-prebuilds';
 import { Stream } from 'stream';
 import bindingOptions from './binding-options';
+import * as path from 'path';
 
 interface ParquetReader
 {
   readParquet: (path: string, stream: Stream) => void;
 }
 
-export const { readParquet } = bindings<ParquetReader>(__dirname, bindingOptions);
+const rootPath = path.basename(__dirname) == 'dist' ? path.join(__dirname, '..') : __dirname;
+
+export const { readParquet } = bindings<ParquetReader>(rootPath, bindingOptions);
